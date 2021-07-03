c4409ee34d68d2ac82339c793e0958153ef5f952
oguzthmc <47873038+oguzthmc@users.noreply.github.com>
2021-07-03 13:50:50 +0300

    signing up & logging in

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { useContext } from 'react'
 import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
-  const user = useContext(AuthContext)
+  const { user, login } = useContext(AuthContext)
   console.log(user)
 
   return (
@@ -15,6 +15,7 @@ export default function Navbar() {
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li onClick={login} className="btn">Login/Signup</li>
         </ul>
       </nav>
       <div className="banner">
